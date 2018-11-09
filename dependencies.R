de313199914f2c8f17855d52581e50224d9511bb
Irene Liampa <irini.liampa@gmail.com>
2018-11-09 18:38:00 +0200

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -8,7 +8,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 	BiocManager::install("HDF5Array", version = "3.8", update = TRUE, ask = FALSE)
 	BiocManager::install("GenomicRanges", version = "3.8", update = TRUE, ask = FALSE)
 	BiocManager::install("biomaRt", version = "3.8", update = TRUE, ask = FALSE)
-	BiocManager::install('rafalab/bumphunter')
+	BiocManager::install('rafalab/bumphunter', ref = "RELEASE_3_8")
 	BiocManager::install("minfi", version = "3.8", update = TRUE, ask = FALSE)
 	BiocManager::install("conumee", version = "3.8", update = TRUE, ask = FALSE)	
 }
